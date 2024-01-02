d28039c32aad8c06fe81e113b6ab97b51f3af598
Varun <155068775+var22un@users.noreply.github.com>
2024-01-02 11:13:20 +0530

    Add files via upload

---
+++ js/page.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#image").click(function(){
+        $("#img_p").show(2000);
+    });
+});
+
+$(document).ready(function(){
+    $("#image").click(function(){
+        $("#active").hide(2000);
+    });
+});
+
+$(document).ready(function(){
+    $("#act_v").click(function(){
+        $("#active").show(2000);
+    });
+});
+
+$(document).ready(function(){
+    $("#act_v").click(function(){
+        $("#img_p").hide(2000);
+    });
+});
